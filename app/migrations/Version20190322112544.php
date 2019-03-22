fe8a1588134fdb71e9db0b05a4e81293c0f9a3ac
Jeremie Samson <jeremie.samson76@gmail.com>
2019-03-22 11:34:48 +0100

    [Assessor] Add entity and migration

---
+++ app/migrations/Version20190322112544.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20190322112544 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE vote_place (
+          id INT AUTO_INCREMENT NOT NULL, 
+          name VARCHAR(255) NOT NULL, 
+          address VARCHAR(150) NOT NULL, 
+          postal_code VARCHAR(15) DEFAULT NULL, 
+          city_insee VARCHAR(15) DEFAULT NULL, 
+          country VARCHAR(2) NOT NULL, 
+          created_at DATETIME NOT NULL, 
+          updated_at DATETIME NOT NULL, 
+          PRIMARY KEY(id)
+        ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE vote_place');
+    }
+}
---
+++ src/Entity/VotePlace.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Algolia\AlgoliaSearchBundle\Mapping\Annotation as Algolia;
+use AppBundle\Validator\UnitedNationsCountry as AssertUnitedNationsCountry;
+
+/**
+ * VotePlace
+ *
+ * @ORM\Table(name="vote_place")
+ * @ORM\Entity
+ *
+ * @Algolia\Index(autoIndex=false)
+ */
+class VotePlace
+{
+    use EntityTimestampableTrait;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column
+     *
+     * @Assert\NotBlank
+     * @Assert\Length(max=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(length=150)
+     *
+     * @Assert\NotBlank(message="common.address.required")
+     * @Assert\Length(max=150, maxMessage="common.address.max_length")
+     */
+    private $address;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(length=15, nullable=true)
+     *
+     * @Assert\Length(max=15)
+     */
+    private $postalCode;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="city_insee", length=15, nullable=true)
+     *
+     * @Assert\Length(max=15)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(length=2)
+     *
+     * @Assert\NotBlank
+     * @AssertUnitedNationsCountry(message="common.country.invalid")
+     */
+    private $country;
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(?string $postalCode): void
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): void
+    {
+        $this->city = $city;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+}
